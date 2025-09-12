90fdaf3dc1ef72f9c2fdbe6df774f3270c5d8f4a
akih1r <huangyuxiamu@gmail.com>
2025-09-13 03:38:14 +0900

    ABC328 d solution

---
+++ abc328_d.py
@@ -0,0 +1,15 @@
+import sys, bisect, heapq
+from collections import defaultdict, deque, OrderedDict, Counter
+from itertools import combinations
+sys.setrecursionlimit(10**7)
+
+S = list(input())
+
+stack = []
+for ch in S:
+    stack.append(ch)
+    # 直近3文字が "ABC" なら消去（連鎖も while で対応）
+    while len(stack) >= 3 and stack[-3] == 'A' and stack[-2] == 'B' and stack[-1] == 'C':
+        stack.pop(); stack.pop(); stack.pop()
+
+print(''.join(stack))
