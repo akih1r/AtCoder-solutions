6f98146be7b0935357c89e11baf07d816e939e0f
akih1r <huangyuxiamu@gmail.com>
2026-02-07 14:13:57 +0900

    DP 部分和問題

---
+++ abc/abc286_d.py
@@ -0,0 +1,37 @@
+import sys, bisect, heapq
+from collections import defaultdict, deque, OrderedDict, Counter
+from itertools import combinations, permutations, accumulate
+sys.setrecursionlimit(10**7)
+import math
+#N=int(input())
+#N, M, P = map(int,input().split())
+#A = list(map(int,input().split()))
+#B = sorted(list(map(int,input().split())))
+
+#ls = [list(map(int, input().split())) for _ in range(N)]
+#grid = [list(input()) for _ in range(N)]
+#alpha = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+
+import sys
+
+
+data = sys.stdin.read().split()
+iterator = iter(data)
+
+n = int(next(iterator))
+x = int(next(iterator))
+
+
+dp = 1 
+
+for _ in range(n):
+    a = int(next(iterator))
+    b = int(next(iterator))
+    
+    for _ in range(b):
+        dp |= (dp << a)
+
+if (dp >> x) & 1:
+    print("Yes")
+else:
+    print("No")
