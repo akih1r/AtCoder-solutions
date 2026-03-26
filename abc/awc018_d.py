103cfcf3c2161b217f87e0c26b65e38b1b01c183
akih1r <huangyuxiamu@gmail.com>
2026-03-26 16:21:24 +0900

    部分和DP　ビットで

--- abc/awc018_d.py
+++ abc/awc018_d.py
@@ -23,28 +23,24 @@ import math
 
 
 
-N, M, K, T = map(int,input().split())
-C = list(map(int,input().split()))
-set_C = set(C)
-G = defaultdict(list)
-for i in range(M):
-    u, v = map(int,input().split())
-    G[u].append(v)
-    G[v].append(u)
-L = [0]*(N+1)
-for i in range(1,N+1):
-    if i in set_C:
-        L[i] = T
 
-stack = C
-while stack:
-    now = stack.pop()
-    for nxt in G[now]:
-        L[nxt] += 1
-        if L[nxt] == T:
-            stack.append(nxt)
-cnt = 0
-for i in range(1,N+1):
-    if L[i] >= T:
-        cnt += 1
-print(cnt)
+N, K = map(int, input().split())
+C = list(map(int, input().split()))
+
+# dp[j] : 合計金額 j を作ることが可能かどうか（True/False）
+# ここではビット演算を使って高速化します（dpの各ビットが金額に対応）
+dp = 1  # 0ビット目が1（合計0が作れるという意味）
+
+for x in C:
+    # 現在作れる金額すべてに x を足したものを、新たな可能性として追加
+    # (dp << x) は「今までの金額すべてに x を加算した状態」を作る
+    dp |= (dp << x)
+
+# K 以下の範囲で、ビットが立っている最大のインデックスを探す
+# dp.bit_length() - 1 は立っている最大のビット（合計金額）を返しますが、
+# K を超えている可能性があるので、マスク（制限）をかけます
+mask = (1 << (K + 1)) - 1
+possible_sums = dp & mask
+
+# 最も右側（大きい方）のビットの位置が答え
+print(possible_sums.bit_length() - 1)
