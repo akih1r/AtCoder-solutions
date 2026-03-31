c590d34704f4aa66e37f16ed07636e1dc4f73e1c
akih1r <huangyuxiamu@gmail.com>
2026-04-01 02:22:05 +0900

    イベントソート

---
+++ abc/awc036_c.py
@@ -0,0 +1,38 @@
+import sys, bisect, heapq
+from collections import defaultdict, deque, OrderedDict, Counter
+from itertools import combinations, permutations, accumulate
+sys.setrecursionlimit(10**7)
+import math
+
+#=======================================================
+#
+#N=int(input())
+#A =list(map(int,input().split()))
+#S = [0] + accmulate(A)
+#ls = [list(map(int, input().split())) for _ in range(N)]
+#grid = [list(input()) for _ in range(N)]
+#alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+#N, M, T = map(int,input().split())
+#G = defaultdict(list)
+#for i in range(M):
+#    u, v = map(int,input().split())
+#    G[u].append(v)
+#    G[v].append(u)
+#
+#=========================================================
+
+N = int(input())
+events = []
+for _ in range(N):
+  s, e = map(int,input().split())
+  events.append((s, 1))
+  events.append((e, -1))
+
+events.sort()
+ans = 0
+crr = 0
+for _, d in events:
+  crr += d
+  ans = max(ans, crr)
+
+print(ans)
