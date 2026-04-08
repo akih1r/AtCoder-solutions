4bbf38ab74c48dff0a10346b43b6bd7e81aed385
akih1r <huangyuxiamu@gmail.com>
2026-04-08 19:36:29 +0900

    DP 3^n

---
+++ abc/paiza_a_板倒し.py
@@ -0,0 +1,50 @@
+import sys, bisect, heapq
+from collections import defaultdict, deque, OrderedDict, Counter
+from itertools import combinations, permutations, accumulate
+sys.setrecursionlimit(10**7)
+import math
+
+#=======================================================
+#
+#N=int(input())
+#A =list(map(int,input().split()))
+#S = [0] + accmulate(A)
+#ls = [list(map(int, input().split())) for _ in range(N)]
+#grid = [list(input()) for _ in range(N)]
+#alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+#N, M, T = map(int,input().split())
+#G = defaultdict(list)
+#for i in range(M):
+#    u, v = map(int,input().split())
+#    G[u].append(v)
+#    G[v].append(u)
+#
+#=========================================================import heapq
+
+
+
+
+H, W = map(int,input().split())
+grid = [list(map(int,input().split())) for _ in range(H)]
+
+dp = [[-1]* (W) for i in range(H)]
+
+for i in range(W):
+    dp[0][i] = grid[0][i]
+
+for i in range(H-1):
+    for j in range(0, W):
+        if dp[i][j] == -1:
+            continue
+        
+        #下
+        dp[i+1][j] = max(dp[i][j]+ grid[i+1][j], dp[i+1][j])
+        
+        #右下
+        if j+1 < W:
+            dp[i+1][j+1] = max(dp[i][j]+grid[i+1][j+1], dp[i+1][j+1])
+        
+        #左下
+        if j-1 >= 0:
+            dp[i+1][j-1] = max(dp[i][j]+grid[i+1][j-1], dp[i+1][j-1])
+print(max(dp[H-1]))
