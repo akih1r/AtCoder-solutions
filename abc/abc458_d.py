c206ed1d4c4308cae1d85f96814403f8bc09e9bf
akih1r <huangyuxiamu@gmail.com>
2026-05-17 21:11:31 +0900

    sortedlistで管理

---
+++ abc/abc458_d.py
@@ -0,0 +1,37 @@
+import sys, bisect, heapq
+from collections import defaultdict, deque, OrderedDict, Counter
+from itertools import combinations, permutations, accumulate
+sys.setrecursionlimit(10**7)
+import math
+
+#=======================================================
+#
+#N=int(input())
+#A =list(map(int,input().split()))
+#S = [0] + accmulate(A)
+#ls = [list(map(int, input().split())) for _ in range(N)]
+#grid = [list(input()) for _ in range(N)]
+#alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+#N, M, T = map(int,input().split())
+#G = defaultdict(list)
+#for i in range(M):
+#    u, v = map(int,input().split())
+#    G[u].append(v)
+#    G[v].append(u)
+#
+#=========================================================
+
+from sortedcontainers import SortedList
+X = int(input())
+
+q = SortedList()
+Q = int(input())
+
+len_ = 1
+for i in range(Q):
+    a, b = map(int,input().split())
+    mid = len(q) // 2
+    q.add(a)
+    q.add(b)
+    print(q[mid])
+    
